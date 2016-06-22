953966401a4c0dba028e61b0334353f16d25df39
jalalx <sjamini@gmail.com>
2016-06-22 07:04:46 +0430

    cards added

--- Loyalty.Admin/app/configs/routeConfig.js
+++ Loyalty.Admin/app/configs/routeConfig.js
@@ -1,7 +1,7 @@
 ﻿
 angular.module('membersApp')
        .config(function ($routeProvider) {
-           
+
            $routeProvider
            .when("/login", {
                templateUrl: "partials/login.htm",
@@ -24,6 +24,22 @@ angular.module('membersApp')
                templateUrl: "partials/editUser.htm",
                controller: "userscontroller",
                authorize: true,
+           })
+           .when("/editCustomer", {
+               templateUrl: "partials/customer/editCustomer.html",
+               controller: "customercontroller",
+           })
+           .when("/customers", {
+               templateUrl: "partials/customer/customers.html",
+               controller: "customercontroller",
+           })
+           .when("/editCard", {
+               templateUrl: "partials/card/editCard.html",
+               controller: "cardController",
+           })
+           .when("/cards", {
+               templateUrl: "partials/card/cards.html",
+               controller: "cardController",
            })
            .otherwise({
                redirectTo: "/",
